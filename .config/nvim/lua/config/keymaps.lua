94c7bb1b4d0658684c312ca671db9fd2baf8fc8c
Robert Carosi <robert@carosi.nl>
2024-06-25 23:17:33 +0200

    Add zen mode, create project files keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("i", "kj", "<ESC>", { desc = "Exit insert mode with kj" })
+map("n", "<C-b>", ":nohl<CR>", { desc = "Clear search highlights with Ctl-B", silent = true })
+map(
+  "n",
+  "<C-p>",
+  "<cmd>lua require('helpers.project-files').project_files()<CR>",
+  { noremap = true, silent = true, desc = "Find project files" }
+)
