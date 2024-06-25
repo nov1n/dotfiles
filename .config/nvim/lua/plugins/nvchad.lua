52b1e3e1993c727c0b20f811ac50a66818ea71fd
Robert Carosi <robert@carosi.nl>
2024-06-25 22:31:54 +0200

    Replace nvchad with lazyvim

--- .config/nvim/lua/plugins/nvchad.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "NvChad/NvChad",
-  lazy = false,
-  branch = "v2.5",
-  import = "nvchad.plugins",
-}
--- .config/nvim/plugin/options.lua
+++
@@ -1,8 +0,0 @@
-require "nvchad.options"
-
-local o = vim.o
-local opt = vim.opt
-
-o.relativenumber = true
-o.scrolloff = 10 -- Ensure 10 lines under and above cursor
-opt.backspace = { "indent", "eol", "start" } -- Backspace behaves normally
