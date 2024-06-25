eae6287811de8c758a45e60efd959afa01113079
Robert Carosi <robert@carosi.nl>
2024-06-25 23:23:44 +0200

    Add live grep keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,6 +5,7 @@ local map = vim.keymap.set
 
 map("i", "kj", "<ESC>", { desc = "Exit insert mode with kj" })
 map("n", "<C-b>", ":nohl<CR>", { desc = "Clear search highlights with Ctl-B", silent = true })
+map("n", "<C-f>", "<cmd>lua require('telescope.builtin').live_grep()<CR>", { desc = "Live grep in project files" })
 map(
   "n",
   "<C-p>",
