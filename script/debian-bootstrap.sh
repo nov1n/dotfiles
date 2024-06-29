19de718a68c12e14e2a7201d16077e847ad2e1da
Robert Carosi <robert@carosi.nl>
2024-06-29 12:21:08 +0200

    Add dotfiles update alias

--- script/debian-bootstrap.sh
+++ script/debian-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 apt update
-apt install -y git stow zsh curl fzf thefuck zoxide
+apt install -y git stow zsh curl fzf thefuck zoxide lsd bat
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 git clone --depth=1 https://github.com/mattmc3/antidote.git ~/.antidote
 
