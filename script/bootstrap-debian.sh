c07d5c9237321913750f48bfb39eca07909a6f10
Robert Carosi <robert@carosi.nl>
2024-06-29 12:39:07 +0200

    Fix bootstrap script

--- script/bootstrap-debian.sh
+++ script/bootstrap-debian.sh
@@ -1,9 +1,12 @@
 #!/bin/env bash
 
 apt update
-apt install -y git stow zsh curl fzf thefuck zoxide lsd bat
+apt install -y git stow zsh curl thefuck zoxide lsd bat man
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 git clone --depth=1 https://github.com/mattmc3/antidote.git ~/.antidote
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf &&
+	~/.fzf/install --key-bindings --completion --update-rc &&
+	ln -s ~/.fzf/bin/fzf /usr/local/bin/
 
 git clone https://github.com/nov1n/dotfiles.git ~/dotfiles
 cd ~/dotfiles/
