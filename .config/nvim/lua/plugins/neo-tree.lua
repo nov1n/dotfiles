0ae1893a083f67a8f0143a264f90559fa3baf3b7
Robert Carosi <robert@carosi.nl>
2024-06-29 19:45:53 +0200

    Show hidden files in neo-tree

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    close_if_last_window = true,
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+      },
+    },
+  },
+}
