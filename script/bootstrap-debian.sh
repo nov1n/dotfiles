abf170c453f06b74ea4195a06a472e337e0acbb4
Robert Carosi <robert@carosi.nl>
2024-06-29 12:53:53 +0200

    Fix locales

--- script/bootstrap-debian.sh
+++ script/bootstrap-debian.sh
@@ -1,12 +1,15 @@
 #!/bin/env bash
 
 apt update
-apt install -y git stow zsh curl thefuck zoxide lsd bat man
+apt install -y git stow zsh curl thefuck zoxide lsd bat man locales tmux
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 git clone --depth=1 https://github.com/mattmc3/antidote.git ~/.antidote
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf &&
 	~/.fzf/install --key-bindings --completion --no-update-rc &&
 	ln -s ~/.fzf/bin/fzf /usr/local/bin/
+
+sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
 
 git clone https://github.com/nov1n/dotfiles.git ~/dotfiles
 cd ~/dotfiles/
