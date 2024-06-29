4c47dfdae1f56d26f98742edb80e9e0bc8d0a482
Robert Carosi <robert@carosi.nl>
2024-06-29 12:40:58 +0200

    Don't update rc files

--- script/bootstrap-debian.sh
+++ script/bootstrap-debian.sh
@@ -5,7 +5,7 @@ apt install -y git stow zsh curl thefuck zoxide lsd bat man
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 git clone --depth=1 https://github.com/mattmc3/antidote.git ~/.antidote
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf &&
-	~/.fzf/install --key-bindings --completion --update-rc &&
+	~/.fzf/install --key-bindings --completion --no-update-rc &&
 	ln -s ~/.fzf/bin/fzf /usr/local/bin/
 
 git clone https://github.com/nov1n/dotfiles.git ~/dotfiles
