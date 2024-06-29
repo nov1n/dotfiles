86737ed2418c38a7c9bbac87c9f48ecb88570e24
Robert Carosi <robert@carosi.nl>
2024-06-29 12:12:59 +0200

    Update install script

--- script/debian-bootstrap.sh
+++ script/debian-bootstrap.sh
@@ -1,9 +1,12 @@
 #!/bin/env bash
 
 apt update
-apt install -y git stow zsh
+apt install -y git stow zsh curl fzf thefuck zoxide
+curl -sS https://starship.rs/install.sh | sh -s -- -y
+git clone --depth=1 https://github.com/mattmc3/antidote.git ~/.antidote
+
 git clone https://github.com/nov1n/dotfiles.git ~/dotfiles
 cd ~/dotfiles/
 stow -v .
 
-# Set default shell
+chsh -s $(which zsh)
