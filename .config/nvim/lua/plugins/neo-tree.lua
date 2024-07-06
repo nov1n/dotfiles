d17bb9d986479c97bf883d2aa143fce928a872a5
Robert Carosi <robert@carosi.nl>
2024-07-06 20:59:33 +0200

    Use trash instead of rm in neo-tree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,3 +1,6 @@
+local inputs = require("neo-tree.ui.inputs")
+local sources = require("neo-tree.sources.manager")
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
@@ -6,6 +9,39 @@ return {
       filtered_items = {
         visible = true, -- Don't show hidden files in a lighter color
         hide_dotfiles = false, -- Show dotfiles
+      },
+      commands = {
+        -- Overwrite default 'delete' command to 'trash'.
+        delete = function(state)
+          local path = state.tree:get_node().path
+          local msg = "Are you sure you want to trash " .. path
+          inputs.confirm(msg, function(confirmed)
+            if not confirmed then
+              return
+            end
+
+            vim.fn.system({ "trash", path })
+            sources.refresh(state.name)
+          end)
+        end,
+
+        -- Overwrite default 'delete_visual' command to 'trash' x n.
+        delete_visual = function(state, selected_nodes)
+          local count = #selected_nodes
+          local msg = "Are you sure you want to trash " .. count .. " files?"
+
+          inputs.confirm(msg, function(confirmed)
+            if not confirmed then
+              return
+            end
+
+            for _, node in ipairs(selected_nodes) do
+              vim.fn.system({ "trash", node.path })
+            end
+
+            sources.refresh(state.name)
+          end)
+        end,
       },
     },
   },
