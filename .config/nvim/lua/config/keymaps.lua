fe418f4927355ad7c807b4ae60ab34325581bcca
Robert Carosi <robert@carosi.nl>
2024-07-11 21:01:32 +0200

    Remove control keybinds from keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,8 +5,8 @@ local map = vim.keymap.set
 
 map("n", "<Leader>uz", "<cmd>ZenMode<CR>", { desc = "Enter ZenMode", silent = true })
 map("n", "<leader>bn", ":enew<CR>", { desc = "Create new buffer", noremap = true, silent = true })
-map("n", "<C-f>", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true, desc = "Grep in project files" })
-map("n", "<C-p>", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true, desc = "Find project files" })
+map("n", "<leader>.", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true, desc = "Find project files" })
+map("n", "<leader>/", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true, desc = "Grep in project files" })
 
 -- Temporary
 map("n", "<leader>k", "<cmd>g/I AM NOT DONE/d | w | nohl<CR>", { noremap = true, silent = true })
