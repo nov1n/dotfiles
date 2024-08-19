ce903636f69c72c3353c39d310cb3cab83b8823d
Robert Carosi <robert@carosi.nl>
2024-08-19 10:44:18 +0200

    Add statusbar to wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,9 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
+
+-- Modules
 local projects = require("projects")
+local statusbar = require("statusbar")
 
 -- Plugins
 local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
@@ -15,7 +19,8 @@ config.set_environment_variables = {
 }
 
 -- Appearance
-config.enable_tab_bar = false
+--config.enable_tab_bar = false
+config.use_fancy_tab_bar = false
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
 config.color_scheme = "Tokyo Night"
@@ -32,36 +37,14 @@ config.window_padding = {
 -- Keymap
 local leader = "OPT"
 config.keys = {
-	{
-		key = "v",
-		mods = leader,
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "s",
-		mods = leader,
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "x",
-		mods = leader,
-		action = wezterm.action.CloseCurrentPane({ confirm = false }),
-	},
-	{
-		key = "c",
-		mods = leader,
-		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		key = "p",
-		mods = leader,
-		action = projects.choose_project(),
-	},
-	{
-		key = "f",
-		mods = leader,
-		action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
-	},
+	{ key = "v", mods = leader, action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "s", mods = leader, action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "c", mods = leader, action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "x", mods = leader, action = act.CloseCurrentPane({ confirm = false }) },
+	{ key = "n", mods = leader, action = act.ActivateTabRelative(-1) },
+	{ key = "p", mods = leader, action = act.ActivateTabRelative(1) },
+	{ key = "w", mods = leader, action = projects.choose_project() },
+	{ key = "f", mods = leader, action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
 }
 smart_splits.apply_to_config(config, {
 	direction_keys = { "h", "j", "k", "l" },
