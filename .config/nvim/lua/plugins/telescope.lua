8ace91e653c23e2d4ca0620a8750092729a38cfa
Robert Carosi <robert@carosi.nl>
2024-10-26 14:40:15 +0200

    Move ignore patterns for telescope to global .ignore file
    This file is respected by both rg and fd.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,16 +1,3 @@
-local file_ignore_patterns = {
-  -- Files
-  "%.class",
-  "%.lock",
-  "%.lock.json",
-  "%.wasm",
-  -- Directories
-  ".cache/",
-  ".git/",
-  ".github/",
-  ".node_modules/",
-}
-
 return {
   "nvim-telescope/telescope.nvim",
   opts = {
@@ -18,18 +5,6 @@ return {
       sorting_strategy = "ascending", -- display results top->bottom
       layout_config = {
         prompt_position = "top", -- search bar at the top
-      },
-    },
-    pickers = {
-      live_grep = {
-        file_ignore_patterns = file_ignore_patterns,
-        additional_args = function(_)
-          return { "--hidden" }
-        end,
-      },
-      find_files = {
-        file_ignore_patterns = file_ignore_patterns,
-        hidden = true,
       },
     },
   },
