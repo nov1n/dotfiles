e7b58465274293268c54edb2b5d87688b012d2ae
Robert Carosi <robert@carosi.nl>
2024-11-07 18:08:40 +0100

    Maximize wezterm on startup

--- .config/wezterm/handlers.lua
+++ .config/wezterm/handlers.lua
@@ -1,11 +1,7 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 
-wezterm.on("gui-attached", function(domain)
-	-- maximize all displayed windows on startup
-	local workspace = mux.get_active_workspace()
-	for _, window in ipairs(mux.all_windows()) do
-		if window:get_workspace() == workspace then
-			window:gui_window():maximize()
-		end
-	end
+wezterm.on("gui-startup", function()
+	local tab, pane, window = mux.spawn_window({})
+	window:gui_window():maximize()
 end)
