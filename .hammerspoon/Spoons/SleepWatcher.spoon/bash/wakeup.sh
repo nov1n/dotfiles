8ad7e11905c34ffdf0b93966b00e1ec69781a725
Robert Carosi <robert@carosi.nl>
2024-11-16 11:30:23 +0100

    Turn off kitchen when pc wakes

--- .hammerspoon/Spoons/SleepWatcher.spoon/bash/wakeup.sh
+++ .hammerspoon/Spoons/SleepWatcher.spoon/bash/wakeup.sh
@@ -10,3 +10,6 @@ echo "Turning on DAC, speakers, and lights..."
 retry hass-cli service call light.turn_on --arguments entity_id=light.desk_switch
 retry hass-cli service call light.turn_on --arguments entity_id=light.monitor_led_strip
 retry hass-cli service call switch.turn_on --arguments entity_id=switch.desk_speakers
+
+echo "Turning off kitchen..."
+retry hass-cli service call homeassistant.turn_off --arguments area_id=kitchen
