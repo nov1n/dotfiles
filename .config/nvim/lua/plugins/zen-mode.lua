b7c1f3b8703347c4a0cf758a1db5d5dfe20ce91c
Robert Carosi <robert@carosi.nl>
2024-12-28 23:47:07 +0100

    Add wezterm integration to ZenMode

--- .config/nvim/lua/plugins/zen-mode.lua
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -2,6 +2,7 @@ return {
   "folke/zen-mode.nvim",
   opts = {
     window = {
+      width = 80,
       options = {
         signcolumn = "no", -- disable signcolumn
         number = false, -- disable number column
@@ -9,7 +10,25 @@ return {
         cursorline = false, -- disable cursorline
         cursorcolumn = false, -- disable cursor column
         foldcolumn = "0", -- disable fold column
+        spell = false,
         list = false, -- disable whitespace characters
+      },
+    },
+    plugins = {
+      options = {
+        enabled = true,
+        ruler = false, -- disables the ruler text in the cmd line area
+        showcmd = false, -- disables the command in the last line of the screen
+        -- you may turn on/off statusline in zen mode by setting 'laststatus'
+        -- statusline will be shown only if 'laststatus' == 3
+        laststatus = 0, -- turn off the statusline in zen mode
+      },
+      -- this will change the font size on wezterm when in zen mode
+      -- See alse also the Plugins/Wezterm section in this projects README
+      wezterm = {
+        enabled = true,
+        -- can be either an absolute font size or the number of incremental steps
+        font = "+1", -- (10% increase per step)
       },
     },
   },
