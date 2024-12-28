fba934a615e186da010ffb67dca9834652dd7ac2
Robert Carosi <robert@carosi.nl>
2024-12-28 23:46:30 +0100

    Update search and find commands to use current working dirs

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -4,4 +4,9 @@ return {
   opts = function(_, opts)
     opts.grep.rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e"
   end,
+  keys = {
+    -- Override root dir variants with cwd variants
+    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+  },
 }
