98c1deb99da0f05ec8dc8546153cfd34e1c8684a
Robert Carosi <robert@carosi.nl>
2024-12-29 15:38:46 +0100

    Add which-key groups

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,13 +1,9 @@
 return {
   "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts_extend = { "spec" },
   opts = {
-    preset = "helix",
-    defaults = {},
     spec = {
-      { "<leader>a", group = "avante", icon = { icon = "󰙵 ", color = "cyan" } },
-      { "<leader>o", group = "obsidian", icon = { icon = "󰙵 ", color = "black" } },
+      { "<leader>a", group = "avante", icon = { icon = "󰧑 ", color = "cyan" } },
+      { "<leader>o", group = "obsidian", icon = { icon = "󰯊 ", color = "black" } },
     },
   },
 }
