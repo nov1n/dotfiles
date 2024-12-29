818cbb55a88e7da7d45ef25ecb480dcc54a123ff
Robert Carosi <robert@carosi.nl>
2024-12-29 15:38:06 +0100

    Add image preview to fzf-lua

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,9 +1,24 @@
 return {
   "ibhagwan/fzf-lua",
   cmd = "FzfLua",
-  opts = function(_, opts)
-    opts.grep.rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e"
-  end,
+  opts = {
+    grep = {
+      rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e",
+    },
+    previewers = {
+      builtin = {
+        extensions = {
+          -- neovim terminal only supports `viu` block output
+          -- unfortunately this means blurry output until neovim
+          -- supports kitty graphics protocol.
+          -- see https://github.com/ibhagwan/fzf-lua/issues/26#issuecomment-1066503752
+          ["png"] = { "viu", "-b" },
+          ["jpg"] = { "viu", "-b" },
+        },
+      },
+    },
+  },
+
   keys = {
     -- Override root dir variants with cwd variants
     { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
