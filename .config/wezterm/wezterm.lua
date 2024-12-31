002d9c0188fa9cdfc2aebb0697b8704635a68a59
Robert Carosi <robert@carosi.nl>
2024-12-31 19:23:17 +0100

    Toggle terminal with keybind in Wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 -- Modules
-local projects = require("projects")
-require("statusbar")
+--require("projects")
+--require("statusbar")
 require("handlers")
 
 -- Plugins
@@ -29,7 +29,7 @@ config.set_environment_variables = {
 -- Appearance
 --config.enable_tab_bar = false
 config.use_fancy_tab_bar = false
--- config.window_close_confirmation = "NeverPrompt"
+config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
 config.color_scheme = "Tokyo Night"
 -- config.window_background_opacity = 0.1
@@ -81,6 +81,47 @@ config.keys = {
 	-- Map control backspace to Control-w
 	{ key = "Backspace", mods = "CTRL", action = act.SendKey({ key = "w", mods = "CTRL" }) },
 	{ key = "w", mods = "CTRL", action = act.Nop },
+
+	-- Toggles from a terminal pane to Neovim
+	{
+		key = "Backspace",
+		mods = leader,
+		action = wezterm.action_callback(function(win, pane)
+			local is_neovim = pane:get_foreground_process_name():match("nvim$") ~= nil
+			local tab = pane:tab()
+
+			if is_neovim then
+				-- If the current pane is Neovim, try to switch to the next pane in the tab
+				local pane_next = tab:get_pane_direction("Down")
+
+				if not pane_next then
+					-- If there is no next pane, split the current pane vertically
+					pane_next = pane:split({ direction = "Bottom" })
+				end
+
+				pane_next:activate()
+			else
+				-- If the current pane is not Neovim, try to find a Neovim pane
+				local tab_panes = tab:panes()
+				print("tab_panes", tab_panes)
+				local neovim_pane = nil
+				for i = 1, #tab_panes do
+					local tab_pane = tab_panes[i]
+					local process_name = tab_pane:get_foreground_process_name()
+					if process_name:match("nvim$") then
+						print("process_name", process_name)
+						neovim_pane = tab_pane
+					end
+				end
+
+				if neovim_pane then
+					-- If a Neovim pane is found, activate it and zoom the tab
+					neovim_pane:activate()
+					tab:set_zoomed(true)
+				end
+			end
+		end),
+	},
 }
 
 smart_splits.apply_to_config(config, {
