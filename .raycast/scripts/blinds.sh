942bb02f1b27300b8ae2546926d411be618d5023
Robert Carosi <robert@carosi.nl>
2025-04-15 10:15:47 +0200

    Add script to draw blinds

---
+++ .raycast/scripts/blinds.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Blinds
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🪟
+
+zsh -ci 'hass-cli service call cover.close_cover --arguments entity_id=cover.office_blinds'
