5a946356ed8be9ff1a4ef032508cc2a3eda23982
Robert Carosi <robert@carosi.nl>
2025-04-27 19:33:36 +0200

    Add Docker install

--- script/bootstrap-ubuntu.sh
+++ script/bootstrap-ubuntu.sh
@@ -27,7 +27,7 @@ install_packages() {
     echo "Failed to update package lists" exit 1
   }
   echo "Installing packages..."
-  sudo apt install -y --allow-unauthenticated git stow zsh curl thefuck lsd man sudo wget unzip cmake ninja-build gettext nodejs npm python3 imagemagick ripgrep fd-find || {
+  sudo apt install -y --allow-unauthenticated ca-certificates git stow zsh curl thefuck lsd man sudo wget unzip cmake ninja-build gettext nodejs npm python3 imagemagick ripgrep fd-find || {
     echo "Failed to install packages"
     exit 1
   }
@@ -74,6 +74,25 @@ install_fzf() {
     echo "Failed to link fzf"
     exit 1
   }
+}
+
+install_docker() {
+  # Add Docker's official GPG key:
+  sudo apt-get update
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
+    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  newgrp docker
 }
 
 install_neovim() {
@@ -159,6 +178,7 @@ install_neovim
 install_lazygit
 install_delta
 install_rust
+install_docker
 generate_ssh_key
 install_dotfiles
 
