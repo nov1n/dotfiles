de24c2fb044eed9fe76b17f7631ab5ade60887e3
Robert Carosi <robert@carosi.nl>
2025-05-03 11:50:54 +0200

    Use brew prefix for path

--- .zsh_path.sh
+++ .zsh_path.sh
@@ -2,5 +2,5 @@
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/go/bin:$PATH"
-[ -x /opt/homebrew/bin/brew ] && eval "$(/opt/homebrew/bin/brew shellenv)"
+command -v brew >/dev/null 2>&1 && eval "$($(brew --prefix)/bin/brew shellenv)"
 export PATH="/Users/carosi/.rd/bin:$PATH"
