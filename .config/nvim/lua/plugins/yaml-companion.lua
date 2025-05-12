ade0589a29052779a21e81f5f7011010338a6fdd
Robert Carosi <robert@carosi.nl>
2025-05-12 08:44:29 +0200

    Add yaml companion to autodetect Kubernetes jsonschema

---
+++ .config/nvim/lua/plugins/yaml-companion.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "someone-stole-my-name/yaml-companion.nvim",
+    ft = { "yaml" },
+    dependencies = {
+      { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    config = function(_, opts)
+      local cfg = require("yaml-companion").setup(opts)
+      require("lspconfig")["yamlls"].setup(cfg)
+    end,
+  },
+}
