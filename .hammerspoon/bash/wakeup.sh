172d441ad657c16beb0d7888327ef914c82fc6d2
Robert Carosi <robert@carosi.nl>
2025-05-17 12:33:04 +0200

    Turn off sleeping on mac wakeup

--- .hammerspoon/bash/wakeup.sh
+++ .hammerspoon/bash/wakeup.sh
@@ -10,3 +10,4 @@ echo "Turning on DAC, speakers, and lights..."
 retry hass-cli service call light.turn_on --arguments entity_id=light.desk_switch
 retry hass-cli service call light.turn_on --arguments entity_id=light.monitor_led_strip
 retry hass-cli service call switch.turn_on --arguments entity_id=switch.desk_speakers
+retry hass-cli service call input_boolean.turn_off --arguments entity_id=input_boolean.sleeping
