c4498fec39e48985b2316c6437f3a1b2371ccc86
Robert Carosi <robert@carosi.nl>
2025-05-18 21:59:46 +0200

    Fix desktop workspace script

--- .raycast/scripts/desktop-workspace.sh
+++ .raycast/scripts/desktop-workspace.sh
@@ -8,29 +8,26 @@
 # Optional parameters:
 # @raycast.icon 🖥️
 
-DURATION=0.05
+DURATION=0.2
 
 osascript -e 'quit app "Aerospace"' || true
-sleep $DURATION
 open -a Aerospace
 while ! aerospace list-windows --all >/dev/null 2>&1; do
   sleep $DURATION
 done
-aerospace workspace main >/dev/null 2>&1
-sleep $DURATION
 aerospace layout v_accordion
 sleep $DURATION
-aerospace trigger-binding --mode main alt-cmd-ctrl-b
+aerospace trigger-binding --mode main alt-cmd-ctrl-g
 sleep $DURATION
 aerospace move right
 sleep $DURATION
 aerospace resize width +640
 sleep $DURATION
-aerospace trigger-binding --mode main alt-cmd-ctrl-v
+aerospace trigger-binding --mode main alt-cmd-ctrl-t
 sleep $DURATION
 aerospace join-with right
 sleep $DURATION
-aerospace trigger-binding --mode main alt-cmd-ctrl-t
+aerospace trigger-binding --mode main alt-cmd-ctrl-f
 sleep $DURATION
 aerospace join-with right
 sleep $DURATION
@@ -39,6 +36,3 @@ sleep $DURATION
 aerospace layout tiling
 sleep $DURATION
 aerospace layout v_accordion
-sleep $DURATION
-aerospace focus up
-sleep $DURATION
