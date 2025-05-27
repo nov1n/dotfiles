e69a9ae349595fa736e678c09957cefe2de77658
Robert Carosi <robert@carosi.nl>
2025-05-27 08:36:33 +0200

    Add colorful-menu

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -5,66 +5,61 @@ return {
       "giuxtaposition/blink-cmp-copilot",
     },
   },
-  opts = {
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-      per_filetype = {
-        codecompanion = { "codecompanion" },
-      },
-    },
-    keymap = {
-      preset = "default",
-      ["<C-enter>"] = {},
-      ["<Tab>"] = {},
-      ["<S-Tab>"] = {},
-      ["<Right>"] = { "snippet_forward" },
-      ["<Left>"] = { "snippet_backward" },
-    },
-    completion = {
-      menu = {
-        draw = {
-          treesitter = { "lsp" },
+  config = function()
+    require("blink.cmp").setup({
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        per_filetype = {
+          codecompanion = { "codecompanion" },
         },
       },
-    },
-    enabled = function()
-      return not string.match(U.get_buf_path(), "/Journal/")
-    end,
-    appearance = {
-      -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
-      kind_icons = {
-        Copilot = "",
-        Text = "󰉿",
-        Method = "󰊕",
-        Function = "󰊕",
-        Constructor = "󰒓",
-
-        Field = "󰜢",
-        Variable = "󰆦",
-        Property = "󰖷",
-
-        Class = "󱡠",
-        Interface = "󱡠",
-        Struct = "󱡠",
-        Module = "󰅩",
-
-        Unit = "󰪚",
-        Value = "󰦨",
-        Enum = "󰦨",
-        EnumMember = "󰦨",
-
-        Keyword = "󰻾",
-        Constant = "󰏿",
-
-        Snippet = "󱄽",
-        Color = "󰏘",
-        File = "󰈔",
-        Reference = "󰬲",
-        Folder = "󰉋",
-        Event = "󱐋",
-        Operator = "󰪚",
-        TypeParameter = "󰬛",
+      keymap = {
+        preset = "default",
+        ["<C-enter>"] = {},
+        ["<Tab>"] = {},
+        ["<S-Tab>"] = {},
+        ["<Right>"] = { "snippet_forward" },
+        ["<Left>"] = { "snippet_backward" },
       },
-    },
-  },
+      enabled = function()
+        return not string.match(U.get_buf_path(), "/Journal/")
+      end,
+      appearance = {
+        -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
+        kind_icons = {
+          Copilot = "",
+          Text = "󰉿",
+          Method = "󰊕",
+          Function = "󰊕",
+          Constructor = "󰒓",
+
+          Field = "󰜢",
+          Variable = "󰆦",
+          Property = "󰖷",
+
+          Class = "󱡠",
+          Interface = "󱡠",
+          Struct = "󱡠",
+          Module = "󰅩",
+
+          Unit = "󰪚",
+          Value = "󰦨",
+          Enum = "󰦨",
+          EnumMember = "󰦨",
+
+          Keyword = "󰻾",
+          Constant = "󰏿",
+
+          Snippet = "󱄽",
+          Color = "󰏘",
+          File = "󰈔",
+          Reference = "󰬲",
+          Folder = "󰉋",
+          Event = "󱐋",
+          Operator = "󰪚",
+          TypeParameter = "󰬛",
+        },
+      },
+    })
+  end,
 }
