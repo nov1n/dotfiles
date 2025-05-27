d022b66376ee86a10e0de85624a9835236fece39
Robert Carosi <robert@carosi.nl>
2025-05-27 11:01:14 +0200

    Replace clock with cwd in lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,10 +3,8 @@ return {
   opts = function(_, opts)
     -- Show full relative path
     opts.sections.lualine_c[4] = { "filename", path = 1 }
-    -- Disable clock
-    opts.sections.lualine_z = {}
-    -- Add cwd to right side
-    opts.sections.lualine_x = {
+    -- Replace clock with cwd
+    opts.sections.lualine_z = {
       function()
         return " " .. vim.fn.getcwd()
       end,
