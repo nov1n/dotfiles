4633927057177b8dbc63563fe6c77eff2515c874
Robert Carosi <robert@carosi.nl>
2025-05-31 16:27:06 +0200

    Fix rust keymap

--- .config/nvim/after/ftplugin/rust.lua
+++ .config/nvim/after/ftplugin/rust.lua
@@ -3,12 +3,11 @@ local map = vim.keymap.set
 map("n", "J", "<cmd>RustLsp joinLines<CR>", { noremap = true, silent = true })
 
 map("n", "<leader>xe", "<cmd>RustLsp explainError<CR>", { noremap = true, silent = true, desc = "Explain error" })
-map("n", "<leader>xd", "<cmd>RustLsp openDocs<CR>", { noremap = true, silent = true, desc = "Open docs" })
+map("n", "<leader>xk", "<cmd>RustLsp openDocs<CR>", { noremap = true, silent = true, desc = "Open docs" })
 map("n", "xm", "<cmd>RustLsp expandMacro<CR>", { noremap = true, silent = true, desc = "Expand macro" })
-map("n", "]d", "<cmd>RustLsp renderDiagnostic cycle<CR>", { noremap = true, silent = true, desc = "Render diagnostic" })
 map(
   "n",
-  "[d",
-  "<cmd>RustLsp renderDiagnostic cycle_prev<CR>",
+  "<leader>xd",
+  "<cmd>RustLsp renderDiagnostic current<CR>",
   { noremap = true, silent = true, desc = "Render diagnostic" }
 )
