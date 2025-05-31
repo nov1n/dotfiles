7aff155d42de224775d0bdfff27ab92620cfb6f5
Robert Carosi <robert@carosi.nl>
2025-05-31 16:27:48 +0200

    Fix clone repo script

--- .raycast/scripts/clone-repo.sh
+++ .raycast/scripts/clone-repo.sh
@@ -4,7 +4,7 @@
 #
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Clone Git repository in ~/Projects directory
+# @raycast.title Clone Git repository in ~/Projects
 # @raycast.mode compact
 
 # Optional parameters:
@@ -16,8 +16,11 @@ REPO_URL="${1:-$(osascript "$HOME/.local/bin/get_url_from_focused_browser.scpt")
 DOMAIN="${2:-personal}"
 TARGET_DIR="${2:-"$HOME/Projects/$DOMAIN"}"
 
-REPO_PATH=$(echo "$REPO_URL" | sed -E 's|^https?://github\.com/([^/]+)/([^/#]+).*|\1/\2|')
-TARGET_PATH="$TARGET_DIR/${REPO_PATH}"
+# Extract the full repository path (e.g., "foo/bar") and the repository name (e.g., "bar")
+REPO_PATH=$(echo "$REPO_URL" | sed -E 's|^https?://github\.com/([^/]+/[^/#]+).*|\1|')
+REPO_NAME=$(basename "$REPO_PATH")
+TARGET_PATH="$TARGET_DIR/$REPO_NAME"
+
 if [ ! -d "$TARGET_PATH" ]; then
   git clone "git@github.com:${REPO_PATH}.git" "$TARGET_PATH"
 fi
