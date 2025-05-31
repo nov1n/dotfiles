dbedb6d840bfaa65324e1010a2e154ccfe261e19
Robert Carosi <robert@carosi.nl>
2025-05-31 16:27:17 +0200

    Add new pickers

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
+local Snacks = require("snacks")
+
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
@@ -18,8 +20,25 @@ local map = function(modes, lhs, rhs, desc, options)
   return vim.keymap.set(modes, lhs, rhs, opts)
 end
 
+local project_picker = function()
+  Snacks.picker.projects({
+    dev = { "~/Projects/personal", "~/Projects/picnic" },
+    projects = { "~/dotfiles" },
+    recent = false,
+    win = {
+      preview = { minimal = true },
+      input = {
+        keys = {
+          ["<C-w>"] = { "<c-s-w>", mode = { "i" }, expr = true, desc = "delete word" },
+        },
+      },
+    },
+  })
+end
+
 -- stylua: ignore start
 --                                 HACK: The vim.schedule was needed, because the plugin stopped working at some point. Figure out why.
+map({"n"},          "<leader><space>", function() Snacks.picker.smart() end,                                   "Smart Find Files")
 map({ "n" },        "<M-h>",      function() vim.schedule(require("smart-splits").move_cursor_left) end,       "Move left")
 map({ "n" },        "<M-j>",      function() vim.schedule(require("smart-splits").move_cursor_down) end,       "Move down")
 map({ "n" },        "<M-k>",      function() vim.schedule(require("smart-splits").move_cursor_up) end,         "Move up")
@@ -45,12 +64,14 @@ map({ "n", "v" },   "<leader>aa", "<cmd>CodeCompanionActions<cr>",              
 map({ "n", "v" },   "<leader>at", "<cmd>CodeCompanionChat Toggle<cr>",                                         "Toggle CodeCompanion chat")
 map({ "n", "v" },   "<leader>an", "<cmd>CodeCompanionChat<cr>",                                                "New CodeCompanion chat")
 map({ "n", "v" },   "<leader>ap", ":CodeCompanion<cr>",                                                        "Prompt CodeCompanion")
-map({ "n" },        "<leader>fd", function() Snacks.picker.files({cwd = "~/dotfiles"}) end,                    "Find dotfiles")
 map({ "v" },        "<leader>av", "<cmd>CodeCompanionChat Add<cr>",                                            "Add current selection to CodeCompanion chat")
 map({ "n", "x" },   "j",          function() return vim.v.count > 1 and "m'" .. vim.v.count .. "j" or "j" end, "Add <count>j to jumplist")
 map({ "n", "x" },   "k",          function() return vim.v.count > 1 and "m'" .. vim.v.count .. "k" or "k" end, "Add <count>k to jumplist")
 map({ "n" },        "-",          "<cmd>Oil<cr>",                                                              "Open parent directory in oil")
 map({ "n" },        "<leader>E",  "<cmd>:LazyExtras<cr>",                                                      "Open Lazy Extras panel")
+map({ "n" },        "<leader>fd", function() Snacks.picker.files({cwd = "~/dotfiles"}) end,                    "Find dotfiles")
+map({ "n" },        "<leader>fp", project_picker,                                                              "Find projects")
+map({ "n", "i" },   "<M-cr>",     vim.lsp.buf.code_action,                                                     "Perform code action")
 -- stylua: ignore end
 --
 -- HACK: For some reason this mappings works only with vimscript
