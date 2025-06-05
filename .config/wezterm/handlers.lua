ea544153732b65b1b16527131cee55b3d994f84a
Robert Carosi <robert@carosi.nl>
2025-06-05 14:17:14 +0200

    Add wezterm stylua and change font

--- .config/wezterm/handlers.lua
+++ .config/wezterm/handlers.lua
@@ -5,53 +5,53 @@ local act = wezterm.action
 
 -- Notify when config reloads
 wezterm.on("window-config-reloaded", function(window, pane)
-	window:toast_notification("Wezterm", "Configuration reloaded!", nil, 4000)
+  window:toast_notification("Wezterm", "Configuration reloaded!", nil, 4000)
 end)
 
 wezterm.on("user-var-changed", function(window, pane, name, value)
-	if name ~= "NOTIFICATION" then
-		return
-	end
+  if name ~= "NOTIFICATION" then
+    return
+  end
 
-	local notification = wezterm.json_parse(value)
+  local notification = wezterm.json_parse(value)
 
-	if notification.check_focus and window:is_focused() and window:active_pane():pane_id() == pane:pane_id() then
-		return
-	end
+  if notification.check_focus and window:is_focused() and window:active_pane():pane_id() == pane:pane_id() then
+    return
+  end
 
-	if not notification.timeout_milliseconds then
-		notification.timeout_milliseconds = nil
-	end
+  if not notification.timeout_milliseconds then
+    notification.timeout_milliseconds = nil
+  end
 
-	window:toast_notification(notification.title, notification.message, nil, notification.timeout_milliseconds)
+  window:toast_notification(notification.title, notification.message, nil, notification.timeout_milliseconds)
 end)
 
 -- Open scrollback in nvim
 wezterm.on("trigger-vim-with-scrollback", function(window, pane)
-	-- Retrieve the text from the pane
-	local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
+  -- Retrieve the text from the pane
+  local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
 
-	-- Create a temporary file to pass to vim
-	local name = os.tmpname()
-	local f = io.open(name, "w+")
-	f:write(text)
-	f:flush()
-	f:close()
+  -- Create a temporary file to pass to vim
+  local name = os.tmpname()
+  local f = io.open(name, "w+")
+  f:write(text)
+  f:flush()
+  f:close()
 
-	-- Open a new window running vim and tell it to open the file
-	window:perform_action(
-		act.SpawnCommandInNewWindow({
-			args = { "nvim", name },
-		}),
-		pane
-	)
+  -- Open a new window running vim and tell it to open the file
+  window:perform_action(
+    act.SpawnCommandInNewWindow({
+      args = { "nvim", name },
+    }),
+    pane
+  )
 
-	-- Wait "enough" time for vim to read the file before we remove it.
-	-- The window creation and process spawn are asynchronous wrt. running
-	-- this script and are not awaitable, so we just pick a number.
-	--
-	-- Note: We don't strictly need to remove this file, but it is nice
-	-- to avoid cluttering up the temporary directory.
-	wezterm.sleep_ms(1000)
-	os.remove(name)
+  -- Wait "enough" time for vim to read the file before we remove it.
+  -- The window creation and process spawn are asynchronous wrt. running
+  -- this script and are not awaitable, so we just pick a number.
+  --
+  -- Note: We don't strictly need to remove this file, but it is nice
+  -- to avoid cluttering up the temporary directory.
+  wezterm.sleep_ms(1000)
+  os.remove(name)
 end)
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,7 +14,7 @@ require("handlers")
 local config = wezterm.config_builder()
 config.automatically_reload_config = true
 config.set_environment_variables = {
-	PATH = "/opt/homebrew/bin:" .. os.getenv("PATH"),
+  PATH = "/opt/homebrew/bin:" .. os.getenv("PATH"),
 }
 
 --          ╭─────────────────────────────────────────────────────────╮
@@ -34,16 +34,16 @@ config.use_fancy_tab_bar = false
 config.notification_handling = "AlwaysShow"
 config.window_decorations = "RESIZE"
 config.color_scheme = "Tokyo Night Moon"
-config.font = wezterm.font("0xProto Nerd Font")
-config.harfbuzz_features = { "ss01" } -- Enable cursive comments
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+-- config.harfbuzz_features = { "ss01" } -- Enable cursive comments
 config.font_size = 21
 config.tab_max_width = 300
 config.show_new_tab_button_in_tab_bar = false
 config.window_padding = {
-	left = 0,
-	right = 20,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 20,
+  top = 0,
+  bottom = 0,
 }
 
 --          ╭─────────────────────────────────────────────────────────╮
@@ -53,127 +53,127 @@ config.window_padding = {
 config.disable_default_key_bindings = true
 local mod = "CMD"
 config.keys = {
-	-- System
-	{ key = "x", mods = mod, action = act.ShowDebugOverlay },
-	{ key = "q", mods = mod, action = act.QuitApplication },
+  -- System
+  { key = "x", mods = mod, action = act.ShowDebugOverlay },
+  { key = "q", mods = mod, action = act.QuitApplication },
 
-	-- Tabs
-	{ key = "t", mods = mod, action = act.SpawnTab("CurrentPaneDomain") },
-	{ key = "1", mods = mod, action = act.ActivateTab(0) },
-	{ key = "2", mods = mod, action = act.ActivateTab(1) },
-	{ key = "3", mods = mod, action = act.ActivateTab(2) },
-	{ key = "4", mods = mod, action = act.ActivateTab(3) },
-	{ key = "5", mods = mod, action = act.ActivateTab(4) },
-	{ key = "6", mods = mod, action = act.ActivateTab(5) },
-	{ key = "7", mods = mod, action = act.ActivateTab(6) },
-	{ key = "8", mods = mod, action = act.ActivateTab(7) },
-	{ key = "9", mods = mod, action = act.ActivateTab(8) },
-	{ key = "[", mods = mod, action = act.ActivateTabRelative(-1) },
-	{ key = "]", mods = mod, action = act.ActivateTabRelative(1) },
-	{ key = "{", mods = mod, action = act.MoveTabRelative(-1) },
-	{ key = "}", mods = mod, action = act.MoveTabRelative(1) },
-	-- Panes
-	{ key = "w", mods = mod, action = act.CloseCurrentPane({ confirm = true }) },
-	{ key = "d", mods = mod, action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "D", mods = mod, action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = "z", mods = mod, action = act.TogglePaneZoomState },
+  -- Tabs
+  { key = "t", mods = mod, action = act.SpawnTab("CurrentPaneDomain") },
+  { key = "1", mods = mod, action = act.ActivateTab(0) },
+  { key = "2", mods = mod, action = act.ActivateTab(1) },
+  { key = "3", mods = mod, action = act.ActivateTab(2) },
+  { key = "4", mods = mod, action = act.ActivateTab(3) },
+  { key = "5", mods = mod, action = act.ActivateTab(4) },
+  { key = "6", mods = mod, action = act.ActivateTab(5) },
+  { key = "7", mods = mod, action = act.ActivateTab(6) },
+  { key = "8", mods = mod, action = act.ActivateTab(7) },
+  { key = "9", mods = mod, action = act.ActivateTab(8) },
+  { key = "[", mods = mod, action = act.ActivateTabRelative(-1) },
+  { key = "]", mods = mod, action = act.ActivateTabRelative(1) },
+  { key = "{", mods = mod, action = act.MoveTabRelative(-1) },
+  { key = "}", mods = mod, action = act.MoveTabRelative(1) },
+  -- Panes
+  { key = "w", mods = mod, action = act.CloseCurrentPane({ confirm = true }) },
+  { key = "d", mods = mod, action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "D", mods = mod, action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "z", mods = mod, action = act.TogglePaneZoomState },
 
-	-- Copy, paste, search
-	{ key = "c", mods = mod, action = act.CopyTo("Clipboard") },
-	{ key = "v", mods = mod, action = act.PasteFrom("Clipboard") },
-	{ key = "f", mods = mod, action = act.Search("CurrentSelectionOrEmptyString") },
+  -- Copy, paste, search
+  { key = "c", mods = mod, action = act.CopyTo("Clipboard") },
+  { key = "v", mods = mod, action = act.PasteFrom("Clipboard") },
+  { key = "f", mods = mod, action = act.Search("CurrentSelectionOrEmptyString") },
 
-	-- Font size
-	{ key = "-", mods = mod, action = act.DecreaseFontSize },
-	{ key = "=", mods = mod, action = act.IncreaseFontSize },
-	{ key = "0", mods = mod, action = act.ResetFontSize },
+  -- Font size
+  { key = "-", mods = mod, action = act.DecreaseFontSize },
+  { key = "=", mods = mod, action = act.IncreaseFontSize },
+  { key = "0", mods = mod, action = act.ResetFontSize },
 
-	-- Map control backspace to Control-w
-	{ key = "Backspace", mods = "CTRL", action = act.SendKey({ key = "w", mods = "CTRL" }) },
-	{ key = "w", mods = "CTRL", action = act.Nop },
+  -- Map control backspace to Control-w
+  { key = "Backspace", mods = "CTRL", action = act.SendKey({ key = "w", mods = "CTRL" }) },
+  { key = "w", mods = "CTRL", action = act.Nop },
 
-	-- Scrolling
-	{ key = "UpArrow", mods = "SHIFT", action = act.ScrollByLine(-5) },
-	{ key = "DownArrow", mods = "SHIFT", action = act.ScrollByLine(5) },
-	{ key = "PageUp", action = act.ScrollByPage(-0.5) },
-	{ key = "PageDown", action = act.ScrollByPage(0.5) },
-	{ key = "e", mods = mod, action = act.EmitEvent("trigger-vim-with-scrollback") },
+  -- Scrolling
+  { key = "UpArrow", mods = "SHIFT", action = act.ScrollByLine(-5) },
+  { key = "DownArrow", mods = "SHIFT", action = act.ScrollByLine(5) },
+  { key = "PageUp", action = act.ScrollByPage(-0.5) },
+  { key = "PageDown", action = act.ScrollByPage(0.5) },
+  { key = "e", mods = mod, action = act.EmitEvent("trigger-vim-with-scrollback") },
 
-	-- Toggles from a terminal pane to Neovim
-	{
-		key = "Backspace",
-		mods = mod,
-		action = wezterm.action_callback(function(_, pane)
-			local is_neovim = pane:get_foreground_process_name():match("nvim$") ~= nil
-			local tab = pane:tab()
+  -- Toggles from a terminal pane to Neovim
+  {
+    key = "Backspace",
+    mods = mod,
+    action = wezterm.action_callback(function(_, pane)
+      local is_neovim = pane:get_foreground_process_name():match("nvim$") ~= nil
+      local tab = pane:tab()
 
-			if is_neovim then
-				-- If the current pane is Neovim, try to switch to the next pane in the tab
-				local pane_next = tab:get_pane_direction("Down")
+      if is_neovim then
+        -- If the current pane is Neovim, try to switch to the next pane in the tab
+        local pane_next = tab:get_pane_direction("Down")
 
-				if not pane_next then
-					-- If there is no next pane, split the current pane vertically
-					pane_next = pane:split({ direction = "Bottom", cwd = pane:get_current_working_dir().file_path })
-				end
+        if not pane_next then
+          -- If there is no next pane, split the current pane vertically
+          pane_next = pane:split({ direction = "Bottom", cwd = pane:get_current_working_dir().file_path })
+        end
 
-				pane_next:activate()
-			else
-				-- If the current pane is not Neovim, try to find a Neovim pane
-				local tab_panes = tab:panes()
-				local neovim_pane = nil
-				for i = 1, #tab_panes do
-					local tab_pane = tab_panes[i]
-					local process_name = tab_pane:get_foreground_process_name()
-					if process_name:match("nvim$") then
-						neovim_pane = tab_pane
-					end
-				end
+        pane_next:activate()
+      else
+        -- If the current pane is not Neovim, try to find a Neovim pane
+        local tab_panes = tab:panes()
+        local neovim_pane = nil
+        for i = 1, #tab_panes do
+          local tab_pane = tab_panes[i]
+          local process_name = tab_pane:get_foreground_process_name()
+          if process_name:match("nvim$") then
+            neovim_pane = tab_pane
+          end
+        end
 
-				if neovim_pane then
-					-- If a Neovim pane is found, activate it and zoom the tab
-					neovim_pane:activate()
-					tab:set_zoomed(true)
-				end
-			end
-		end),
-	},
+        if neovim_pane then
+          -- If a Neovim pane is found, activate it and zoom the tab
+          neovim_pane:activate()
+          tab:set_zoomed(true)
+        end
+      end
+    end),
+  },
 
-	-- Rerun last command in pane below
-	{
-		key = "r",
-		mods = mod,
-		action = wezterm.action_callback(function(win, pane)
-			local is_neovim = pane:get_foreground_process_name():match("nvim$") ~= nil
-			local tab = pane:tab()
+  -- Rerun last command in pane below
+  {
+    key = "r",
+    mods = mod,
+    action = wezterm.action_callback(function(win, pane)
+      local is_neovim = pane:get_foreground_process_name():match("nvim$") ~= nil
+      local tab = pane:tab()
 
-			if is_neovim then
-				-- If the current pane is Neovim, try to switch to the next pane in the tab
-				local pane_next = tab:get_pane_direction("Down")
+      if is_neovim then
+        -- If the current pane is Neovim, try to switch to the next pane in the tab
+        local pane_next = tab:get_pane_direction("Down")
 
-				if not pane_next then
-					-- If there is no next pane, we do nothing
-					print("No pane below the current one!")
-					return
-				end
+        if not pane_next then
+          -- If there is no next pane, we do nothing
+          print("No pane below the current one!")
+          return
+        end
 
-				pane_next:activate()
+        pane_next:activate()
 
-				win:perform_action(act({ SendString = "\x03!!\n" }), pane_next)
+        win:perform_action(act({ SendString = "\x03!!\n" }), pane_next)
 
-				-- Return focus to the original pane
-				win:perform_action(act({ ActivatePaneDirection = "Up" }), pane)
-			end
-		end),
-	},
+        -- Return focus to the original pane
+        win:perform_action(act({ ActivatePaneDirection = "Up" }), pane)
+      end
+    end),
+  },
 }
 
 local cwd = {
-	"cwd",
-	padding = 0,
-	max_length = 30,
-	fmt = function(cwd)
-		return tostring(cwd):gsub("carosi", "~"):match("([^/]+)/?$")
-	end,
+  "cwd",
+  padding = 0,
+  max_length = 30,
+  fmt = function(cwd)
+    return tostring(cwd):gsub("carosi", "~"):match("([^/]+)/?$")
+  end,
 }
 
 --          ╭─────────────────────────────────────────────────────────╮
@@ -185,44 +185,44 @@ local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabl
 
 -- Tabline
 tabline.setup({
-	options = {
-		theme = "Tokyo Night Moon",
-		theme_overrides = {
-			tab = {
-				active = { fg = "#819EFA", bg = "#353955" },
-				inactive = { fg = "#797FAD", bg = "#1C1D2A" },
-				inactive_hover = { fg = "#797FAD", bg = "#353955" },
-			},
-		},
-	},
-	sections = {
-		tabline_a = { "mode" },
-		tabline_b = {},
-		tabline_c = { " " },
-		tab_active = {
-			"index",
-			cwd,
-			{
-				"process",
-				icons_only = true,
-			},
-			{ "zoomed", padding = 0 },
-		},
-		tab_inactive = { "index", cwd, { "process", icons_only = true } },
-		tabline_x = {},
-		tabline_y = {},
-		tabline_z = {},
-	},
-	extensions = {},
+  options = {
+    theme = "Tokyo Night Moon",
+    theme_overrides = {
+      tab = {
+        active = { fg = "#819EFA", bg = "#353955" },
+        inactive = { fg = "#797FAD", bg = "#1C1D2A" },
+        inactive_hover = { fg = "#797FAD", bg = "#353955" },
+      },
+    },
+  },
+  sections = {
+    tabline_a = { "mode" },
+    tabline_b = {},
+    tabline_c = { " " },
+    tab_active = {
+      "index",
+      cwd,
+      {
+        "process",
+        icons_only = true,
+      },
+      { "zoomed", padding = 0 },
+    },
+    tab_inactive = { "index", cwd, { "process", icons_only = true } },
+    tabline_x = {},
+    tabline_y = {},
+    tabline_z = {},
+  },
+  extensions = {},
 })
 
 -- Smart splits
 smart_splits.apply_to_config(config, {
-	direction_keys = { "h", "j", "k", "l" },
-	modifiers = {
-		move = "META", -- Option key on MacOS. Karabiner maps command to option because nvim can't have cmd key mappings.
-		resize = "META|SHIFT",
-	},
+  direction_keys = { "h", "j", "k", "l" },
+  modifiers = {
+    move = "META", -- Option key on MacOS. Karabiner maps command to option because nvim can't have cmd key mappings.
+    resize = "META|SHIFT",
+  },
 })
 
 return config
