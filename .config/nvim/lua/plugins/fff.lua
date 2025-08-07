559260d5e536a22781c6237d7de16bfb9d748f75
Robert Carosi <robert@carosi.nl>
2025-08-07 11:59:26 +0200

    Use custom prompt

--- .config/nvim/lua/plugins/fff.lua
+++ .config/nvim/lua/plugins/fff.lua
@@ -1,14 +1,7 @@
 return {
   "dmtrKovalenko/fff.nvim",
   build = "cargo build --release",
-  opts = {},
-  keys = {
-    {
-      "<leader><leader>", -- try it if you didn't it is a banger keybinding for a picker
-      function()
-        require("fff").find_files()
-      end,
-      desc = "FFFind FFFiles",
-    },
+  opts = {
+    prompt = " ",
   },
 }
