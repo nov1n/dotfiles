ea95ee98057cc493d3c374e8013791b1b8288184
Robert Carosi <robert@carosi.nl>
2025-08-07 12:00:01 +0200

    Improve wakeup script

--- .hammerspoon/bash/wakeup.sh
+++ .hammerspoon/bash/wakeup.sh
@@ -4,13 +4,25 @@
 
 echo "-- WAKE --"
 
+current_hour=$(date +%H)
+if [ "$current_hour" -ge 21 ]; then
+  echo "It's after 9pm, skipping wake-up sequence..."
+  exit 0
+fi
+
 if ! is_at_desk; then
   echo "Not at desk, skipping..."
   exit 0
 fi
 
-echo "Turning on DAC, speakers, and lights..."
+echo "Starting wake-up sequence..."
+hass-cli service call automation.reload # Stops the 'Wake up' automation
+hass-cli service call script.turn_off --arguments entity_id=script.new_script # Stops the 'Wake up light simulator'
 hass-cli service call light.turn_on --arguments entity_id=light.monitor_led_strip
 hass-cli service call light.turn_on --arguments entity_id=light.desk_switch
 hass-cli service call switch.turn_on --arguments entity_id=switch.desk_speakers
 hass-cli service call input_boolean.turn_off --arguments entity_id=input_boolean.sleeping
+hass-cli service call light.turn_off --arguments entity_id=light.night_lamp
+hass-cli service call media_player.turn_off --arguments entity_id=media_player.marantz_cinema_70s
+shortcuts run "Turn off Sleep Mode"
+echo "Finished wake-up sequence."
