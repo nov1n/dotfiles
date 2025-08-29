1922aef77acfb1809ac007e56b80e6ed0c0b96a6
Robert Carosi <robert@carosi.nl>
2025-08-29 22:44:58 +0200

    Replace fff with smart picker

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,11 +5,11 @@ local Snacks = require("snacks")
 -- Add any additional keymaps here
 
 local map = function(modes, lhs, rhs, desc, options)
-  local opts = options or {} -- Default options to empty table
+  local opts = options or {}
 
-  opts.desc = opts.desc or desc -- Add description and silent if not already defined in options
-  opts.silent = opts.silent ~= false and true -- Default to silent unless explicitly set to false
-  opts.noremap = opts.noremap ~= false and true -- Default to noremap unless explicitly set to false
+  opts.desc = opts.desc or desc
+  opts.silent = opts.silent ~= false and true
+  opts.noremap = opts.noremap ~= false and true
 
   return vim.keymap.set(modes, lhs, rhs, opts)
 end
@@ -32,48 +32,62 @@ end
 
 local open_oil_with_preview = function()
   local oil = require("oil")
-  oil.open(nil, { preview = {} }, function(err)
-    if not err then
-      vim.api.nvim_command("vertical resize 40")
-    end
+  oil.open(nil, { preview = {} }, function()
+    vim.api.nvim_command("vertical resize 40")
   end)
 end
 
+local toggle_diffview = function()
+  local diffview = require("diffview.lib")
+  local view = diffview.get_current_view()
+  if view then
+    vim.cmd("DiffviewClose")
+  else
+    vim.fn.feedkeys(":DiffviewOpen ")
+  end
+end
+
+-- WARNING: Incorrect use of '{ expr = true }' will cause the keybind to lazily
+-- evaluate every time it's pressed causing UI lag, use with caution.
+local function smart_movement(direction)
+  return function()
+    return vim.v.count > 1 and "m'" .. vim.v.count .. direction or direction
+  end
+end
+
 -- stylua: ignore start
-map({ "n", "t" },        "<m-h>",      require("smart-splits").move_cursor_left,                               "Move left")
-map({ "n", "t" },        "<m-j>",      require("smart-splits").move_cursor_down,                               "Move down")
-map({ "n", "t" },        "<m-k>",      require("smart-splits").move_cursor_up,                                 "Move up")
-map({ "n", "t" },        "<m-l>",      require("smart-splits").move_cursor_right,                              "Move right")
-map({ "n", "t" },        "<m-H>",      require("smart-splits").resize_left,                                    "Resize left")
-map({ "n", "t" },        "<m-J>",      require("smart-splits").resize_down,                                    "Resize down")
-map({ "n", "t" },        "<m-K>",      require("smart-splits").resize_up,                                      "Resize up")
-map({ "n", "t" },        "<m-L>",      require("smart-splits").resize_right,                                   "Resize right")
-map({ "n" },        "<leader>R", "<cmd>restart<cr>",                                                           "Restart Neovim")
-map({ "n" },        "<leader>do", "<cmd>DiffOrig<cr>",                                                         "Diff buffer with file on disk")
-map({ "n" },        "<leader>bn", ":enew<cr>",                                                                 "Create new buffer")
-map({ "n" },        "<leader>gb", "<cmd>BlameToggle<cr>",                                                      "Git blame")
-map({ "n" },        "<leader>r",  "<cmd>source<cr>",                                                           "Source current file")
-map({ "n" },        "<leader>uz", "<cmd>ZenMode<cr>",                                                          "Enter ZenMode")
-map({ "n", "v" },   "<leader>xr", "<cmd>SnipRun<cr>",                                                          "Run snippet")
-map({ "n", "t" },   "<c-/>",      "<nop",                                                                      "Disable terminal keymap")
-map({ "n", "v" },   "gcb",        "<cmd>CBccbox<cr>",                                                          "Insert box comment")
-map({ "n", "v" },   "gct",        "<cmd>CBllline 15<cr>",                                                      "Insert named parts comment")
-map({ "n" },        "gcl",        "<cmd>CBline<cr>",                                                           "Insert line comment")
-map({ "n", "v" },   "gcm",        "<cmd>CBllbox10<cr>",                                                        "Insert a marked comment")
-map({ "n", "v" },   "gcd",        "<cmd>CBd<cr>",                                                              "Remove a box comment")
--- WARNING: Incorrect use of '{ expr = true }' will cause the keybind to lazily evaluate every time it's pressed causing UI lag, use with caution.
-map({ "n", "x" },   "j",          function() return vim.v.count > 1 and "m'" .. vim.v.count .. "j" or "j" end, "Add <count>j to jumplist", { expr = true })
-map({ "n", "x" },   "k",          function() return vim.v.count > 1 and "m'" .. vim.v.count .. "k" or "k" end, "Add <count>k to jumplist", { expr = true })
-map({ "n" },        "-",          function() open_oil_with_preview() end,                                      "Open parent directory in oil")
-map({ "n" },        "<leader>E",  "<cmd>:LazyExtras<cr>",                                                      "Open Lazy Extras panel")
-map({ "n" },        "<leader>fp", project_picker,                                                              "Find projects")
-map({ "n" },        "<leader>ft", ":vimgrep /^- / %| copen<cr>",                                               "Find todos")
-map({ "n" },        "<leader><leader>", function() require("fff").find_files() end,                            "FFFind FFFiles")
-map({ "n", "i" },   "<m-cr>",     vim.lsp.buf.code_action,                                                     "Perform code action")
-map({ "n" },        "<leader>fd", function() Snacks.picker.files({cwd = "~/dotfiles"}) end,                    "Find dotfiles")
-map({ "n" },        "<leader>fs", function() Snacks.picker.smart() end,                                        "Smart find files")
-map({ "c" },        "<tab>",      "<Nop>",                                                                     "Disable tab in command mode")
-map({ "c" },        "<s-tab>",    "<Nop>",                                                                     "Disable shift tab in command mode")
+map({ "n", "t" },   "<m-h>",            require("smart-splits").move_cursor_left,   "Move left")
+map({ "n", "t" },   "<m-j>",            require("smart-splits").move_cursor_down,   "Move down")
+map({ "n", "t" },   "<m-k>",            require("smart-splits").move_cursor_up,     "Move up")
+map({ "n", "t" },   "<m-l>",            require("smart-splits").move_cursor_right,  "Move right")
+map({ "n", "t" },   "<m-H>",            require("smart-splits").resize_left,        "Resize left")
+map({ "n", "t" },   "<m-J>",            require("smart-splits").resize_down,        "Resize down")
+map({ "n", "t" },   "<m-K>",            require("smart-splits").resize_up,          "Resize up")
+map({ "n", "t" },   "<m-L>",            require("smart-splits").resize_right,       "Resize right")
+map({ "n" },        "<leader>R",        "<cmd>restart<cr>",                         "Restart Neovim")
+map({ "n" },        "<leader>do",       "<cmd>DiffOrig<cr>",                        "Diff buffer with file on disk")
+map({ "n" },        "<leader>bn",       ":enew<cr>",                                "Create new buffer")
+map({ "n" },        "<leader>gb",       "<cmd>BlameToggle<cr>",                     "Git blame")
+map({ "n" },        "<leader>gd",       toggle_diffview,                            "Toggle Diffview")
+map({ "n" },        "<leader>r",        "<cmd>source<cr>",                          "Source current file")
+map({ "n" },        "<leader>uz",       "<cmd>ZenMode<cr>",                         "Enter ZenMode")
+map({ "n", "v" },   "<leader>xr",       "<cmd>SnipRun<cr>",                         "Run snippet")
+map({ "n", "t" },   "<c-/>",            "<nop",                                     "Disable terminal keymap")
+map({ "n", "v" },   "gcb",              "<cmd>CBccbox<cr>",                         "Insert box comment")
+map({ "n", "v" },   "gct",              "<cmd>CBllline 15<cr>",                     "Insert named parts comment")
+map({ "n" },        "gcl",              "<cmd>CBline<cr>",                          "Insert line comment")
+map({ "n", "v" },   "gcm",              "<cmd>CBllbox10<cr>",                       "Insert a marked comment")
+map({ "n", "v" },   "gcd",              "<cmd>CBd<cr>",                             "Remove a box comment")
+map({ "n", "x" },   "j",                smart_movement("j"),                        "Add <count>j to jumplist", { expr = true })
+map({ "n", "x" },   "k",                smart_movement("k"),                        "Add <count>k to jumplist", { expr = true })
+map({ "n" },        "-",                function() open_oil_with_preview() end,     "Open parent directory in oil")
+map({ "n" },        "<leader>E",        "<cmd>:LazyExtras<cr>",                     "Open Lazy Extras panel")
+map({ "n" },        "<leader>fp",       project_picker,                             "Find projects")
+map({ "n" },        "<leader>ft",       ":vimgrep /^- / %| copen<cr>",              "Find todos")
+map({ "n", "i" },   "<m-cr>",           vim.lsp.buf.code_action,                    "Perform code action")
+map({ "n" },        "<leader><leader>", function() Snacks.picker.smart() end,       "Smart find files")
+map({ "c" },        "<tab>",            "<Nop>",                                    "Disable tab in command mode")
+map({ "c" },        "<s-tab>",          "<Nop>",                                    "Disable shift tab in command mode")
 -- stylua: ignore end
 
 -- HACK: For some reason this mappings works only with vimscript
