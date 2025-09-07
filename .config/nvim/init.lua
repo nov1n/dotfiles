17e61351f6d6ff9aca5dc4efcf2e6eb417dbe78f
Robert Carosi <robert@carosi.nl>
2025-09-07 16:31:55 +0200

    More porting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,24 +26,23 @@ local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 _G.Config = {}
 
 -- Options ====================================================================
-vim.g.mapleader      = ' '                                   -- Set leader key to space
-vim.o.iskeyword      = '@,48-57,_,192-255,-'                 -- Treat dash separated words as a word text object
-vim.o.completeopt    = 'menuone,noselect,fuzzy,nosort'       -- Use fuzzy matching for built-in completion
-vim.o.complete       = '.,w,b,kspell'                        -- Use spell check and don't use tags for completion
-vim.o.shiftwidth     = 2                                     -- Use this number of spaces for indentation
-vim.o.tabstop        = 2                                     -- Insert 2 spaces for a tab
-vim.o.autoindent     = true                                  -- Use auto indent
-vim.o.expandtab      = true                                  -- Convert tabs to spaces
-vim.o.formatoptions  = 'rqnl1j'                              -- Improve comment editing
-vim.o.cursorlineopt  = 'screenline,number'                   -- Show cursor line only screen line when wrapped
-vim.o.breakindentopt = 'list:-1'                             -- Add padding for lists when 'wrap' is on
-vim.o.shortmess      = 'CFOSWaco'                            -- Don't show "Scanning..." messages
-vim.o.splitkeep      = 'screen'                              -- Reduce scroll during window split
-vim.o.winborder      = 'rounded'                             -- Use double-line as default border
-vim.o.pumblend       = 10                                    -- Make builtin completion menus slightly transparent
-vim.o.pumheight      = 10                                    -- Make popup menu smaller
-vim.o.winblend       = 10                                    -- Make floating windows slightly transparent
-vim.o.pummaxwidth    = 100                                   -- Limit maximum width of popup menu
+vim.g.mapleader            = ' '                                   -- Set leader key to space
+vim.o.iskeyword            = '@,48-57,_,192-255,-'                 -- Treat dash separated words as a word text object
+vim.o.completeopt          = 'menuone,noselect,fuzzy,nosort'       -- Use fuzzy matching for built-in completion
+vim.o.complete             = '.,w,b,kspell'                        -- Use spell check and don't use tags for completion
+vim.o.shiftwidth           = 2                                     -- Use this number of spaces for indentation
+vim.o.tabstop              = 2                                     -- Insert 2 spaces for a tab
+vim.o.autoindent           = true                                  -- Use auto indent
+vim.o.expandtab            = true                                  -- Convert tabs to spaces
+vim.o.formatoptions        = 'rqnl1j'                              -- Improve comment editing
+vim.o.cursorlineopt        = 'screenline,number'                   -- Show cursor line only screen line when wrapped
+vim.o.breakindentopt       = 'list:-1'                             -- Add padding for lists when 'wrap' is on
+vim.o.shortmess            = 'CFOSWaco'                            -- Don't show "Scanning..." messages
+vim.o.splitkeep            = 'screen'                              -- Reduce scroll during window split
+vim.o.winborder            = 'rounded'                             -- Use double-line as default border
+vim.o.pumheight            = 10                                    -- Make popup menu smaller
+vim.o.winblend             = 10                                    -- Make floating windows slightly transparent
+vim.o.pummaxwidth          = 100                                   -- Limit maximum width of popup menu
 vim.o.completefuzzycollect = 'keyword,files,whole_line'      -- Use fuzzy matching when collecting candidates
 vim.o.listchars            = table.concat({ 'extends:…', 'nbsp:␣', 'precedes:…', 'tab:> ' }, ',') -- Special text symbols
 vim.o.fillchars            = table.concat({ 'foldopen:', 'foldclose:', 'fold: ', 'foldsep: ', 'diff:╱', 'eob: ', }, ',') -- Pretty symbols for folding, diff, and end-of-buffer
@@ -67,12 +66,12 @@ _G.Config.leader_group_clues = {
   { mode = 'n', keys = '<Leader>f', desc = '+Find' },
   { mode = 'n', keys = '<Leader>g', desc = '+Git' },
   { mode = 'n', keys = '<Leader>l', desc = '+LSP' },
-  { zbqr = 'a', xrlf = '<Yrnqre>Y', qrfp = '+Yhn/Ybt' },
-  { zbqr = 'a', xrlf = '<Yrnqre>z', qrfp = '+Znc' },
-  { mode = 'n', keys = '<Leader>o', desc = '+Other' },
+  { mode = 'n', keys = '<Leader>L', desc = '+Lua/Log' },
+  { mode = 'n', keys = '<Leader>m', desc = '+Map' },
+  { mode = 'n', keys = '<Leader>o', desc = '+Obsidian' },
   { mode = 'n', keys = '<Leader>v', desc = '+Visits' },
+  { mode = 'n', keys = '<Leader>x', desc = '+Extra' },
   { mode = 'x', keys = '<Leader>l', desc = '+LSP' },
-  { mode = 'x', keys = '<Leader>r', desc = '+R' },
 }
 
 -- From `mini.basics`
@@ -120,9 +119,9 @@ local xmap_leader = function(suffix, rhs, desc, opts)
   vim.keymap.set('x', '<Leader>' .. suffix, rhs, opts)
 end
 
--- shortcuts of longer commands
-nmap_leader('<leader>', '<Cmd>Pick files<CR>',     'Files')
-nmap_leader('/',        '<Cmd>Pick grep_live<CR>', 'Grep live')
+-- shortcuts
+nmap_leader('<leader>', '<Cmd>Pick files<CR>',                   'Files')
+nmap_leader('/',        '<Cmd>Pick grep_live<CR>',               'Grep live')
 
 -- b is for 'buffer'
 nmap_leader('ba', '<Cmd>b#<CR>',                                 'Alternate')
@@ -165,6 +164,7 @@ nmap_leader('fs', '<Cmd>Pick lsp scope="workspace_symbol"<CR>',      'Symbols wo
 nmap_leader('fS', '<Cmd>Pick lsp scope="document_symbol"<CR>',       'Symbols buffer (LSP)')
 nmap_leader('fv', '<Cmd>Pick visit_paths cwd=""<CR>',                'Visit paths (all)')
 nmap_leader('fV', '<Cmd>Pick visit_paths<CR>',                       'Visit paths (cwd)')
+nmap_leader('fk', '<Cmd>lua Config.pick_keymaps()<CR>',              'Search keymaps' )
 
 -- g is for git
 nmap_leader('gb', '<Cmd>BlameToggle<CR>',                   'Toggle git blame')
@@ -202,18 +202,88 @@ nmap_leader('vV', '<Cmd>lua MiniVisits.remove_label("core")<CR>', 'Remove "core"
 nmap_leader('vl', '<Cmd>lua MiniVisits.add_label()<CR>',          'Add label')
 nmap_leader('vL', '<Cmd>lua MiniVisits.remove_label()<CR>',       'Remove label')
 
--- o is for 'other'
-nmap_leader('oh', '<Cmd>normal gxiagxila<CR>',            'Move arg left')
-nmap_leader('og', '<Cmd>lua MiniDoc.generate()<CR>',      'Generate plugin doc')
-nmap_leader('ol', '<Cmd>normal gxiagxina<CR>',            'Move arg right')
-nmap_leader('oS', '<Cmd>lua Config.insert_section()<CR>', 'Section insert')
-nmap_leader('ot', '<Cmd>lua MiniTrailspace.trim()<CR>',   'Trim trailspace')
-nmap_leader('oz', '<Cmd>lua MiniMisc.zoom()<CR>',         'Zoom toggle')
+-- o is for 'obsidian'
+nmap_leader("o/",  "<cmd>ObsidianSearch<CR>",         "Search notes in vault")
+nmap_leader("ob",  "<cmd>ObsidianBacklinks<CR>",      "Backlinks for current buffer")
+nmap_leader("oc",  "<cmd>ObsidianToggleCheckbox<CR>", "Cycle checkbox state")
+nmap_leader("of",  "<cmd>ObsidianFollowLink<CR>",     "Follow note reference")
+nmap_leader("oja", "<cmd>ObsidianDailies<CR>",        "Open list of daily notes")
+nmap_leader("ojt", "<cmd>ObsidianToday<CR>",          "Open today's note")
+nmap_leader("ojn", "<cmd>ObsidianTomorrow<CR>",       "Open tomorrow's note")
+nmap_leader("ojp", "<cmd>ObsidianYesterday<CR>",      "Open yesterday's note")
+nmap_leader("on",  "<cmd>ObsidianNew<CR>",            "Create a new note")
+nmap_leader("oo",  "<cmd>ObsidianOpen<CR>",           "Open Obsidian")
+nmap_leader("op",  "<cmd>ObsidianPasteImg<CR>",       "Paste clipboard image")
+nmap_leader("or",  "<cmd>ObsidianRename<CR>",         "Rename current note")
+nmap_leader("ol",  "<cmd>ObsidianTemplate<CR>",       "Insert a template")
+nmap_leader("ot",  "<cmd>ObsidianTags<CR>",           "List notes by tag")
+
+-- x is for 'extra'
+nmap_leader('xh', '<Cmd>normal gxiagxila<CR>',              'Move arg left')
+nmap_leader('xg', '<Cmd>lua MiniDoc.generate()<CR>',        'Generate plugin doc')
+nmap_leader('xl', '<Cmd>normal gxiagxina<CR>',              'Move arg right')
+nmap_leader('xS', '<Cmd>lua Config.insert_section()<CR>',   'Section insert')
+nmap_leader('xt', '<Cmd>lua MiniTrailspace.trim()<CR>',     'Trim trailspace')
+nmap_leader('xz', '<Cmd>lua MiniMisc.zoom()<CR>',           'Zoom toggle')
+nmap_leader("xn", '<Cmd>lua MiniNotify.show_history()<CR>', 'Show notifications history')
+
+-- HACK: For some reason this mapping works only with vimscript
+vim.cmd("nmap <bs> ;")
 
 -- Functions ==================================================================
 
 -- Create listed scratch buffer and focus on it
 Config.new_scratch_buffer = function() vim.api.nvim_win_set_buf(0, vim.api.nvim_create_buf(true, true)) end
+
+-- Fuzzy find keymaps
+Config.pick_keymaps = function()
+  local key_modes = { "n", "i", "c", "v", "t" }
+  local entries = {}
+  
+  local function add_keymap(keymap)
+    -- ignore dummy mappings
+    if type(keymap.rhs) == "string" and #keymap.rhs == 0 then
+      return
+    end
+
+    -- ignore <SNR> and <Plug> mappings by default
+    if type(keymap.lhs) == "string" then
+      local lhs = vim.trim(keymap.lhs:lower())
+      if lhs:match("<snr>") or lhs:match("<plug>") then
+        return
+      end
+    end
+
+    local display = string.format("%-1s │ %-20s │ %s",
+      keymap.mode,
+      keymap.lhs:gsub("%s", "<Space>"),
+      keymap.desc or keymap.rhs or tostring(keymap.callback or ""))
+
+    table.insert(entries, { text = display })
+  end
+
+  for _, mode in pairs(key_modes) do
+    local global = vim.api.nvim_get_keymap(mode)
+    for _, keymap in pairs(global) do
+      add_keymap(keymap)
+    end
+    local buf_local = vim.api.nvim_buf_get_keymap(0, mode)
+    for _, keymap in pairs(buf_local) do
+      add_keymap(keymap)
+    end
+  end
+
+  -- sort alphabetically
+  table.sort(entries, function(a, b) return a.text < b.text end)
+
+  local pick = require('mini.pick')
+  pick.start({
+    source = {
+      items = entries,
+      name = "Keymaps",
+    },
+  })
+end
 
 -- Insert section
 Config.insert_section = function(symbol, total_width)
@@ -291,9 +361,39 @@ later(function()
 
   require('nvim-treesitter.configs').setup {
     ensure_installed = {
-      'bash','c','cpp','css','diff','go',
-      'html','javascript','json','julia','nu','php','python',
-      'r','regex','rst','rust','toml','tsx','yaml',
+      "bash",
+      "hcl",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "terraform",
+      "yaml",
+      'bash',
+      'c',
+      'cpp',
+      'css',
+      'diff',
+      'go',
+      'html',
+      'javascript',
+      'json',
+      'julia',
+      'nu',
+      'php',
+      'python',
+      'r',
+      'regex',
+      'rst',
+      'rust',
+      'toml',
+      'tsx',
+      'yaml',
     },
     auto_install = true,
     highlight = { enable = true },
@@ -319,6 +419,23 @@ later(function()
   })
 end)
 
+later(function()
+  add({
+    source = 'folke/lazydev.nvim',
+    depends = {
+      'nvim-lua/plenary.nvim',
+      'gonstoll/wezterm-types',
+    },
+  })
+
+  require('lazydev').setup({
+    library = {
+      { path = 'wezterm-types', mods = { 'wezterm' } },
+    },
+  })
+end)
+
+
 -- Formatting =================================================================
 later(function()
   add('stevearc/conform.nvim')
@@ -343,6 +460,42 @@ later(function()
     },
   })
 end)
+
+later(function()
+  add({
+    source = 'epwalsh/obsidian.nvim',
+    depends = {
+      'nvim-lua/plenary.nvim',
+    },
+  })
+
+  require('obsidian').setup({
+    daily_notes = {
+      folder = 'Journal',
+      default_tags = { 'journal' },
+    },
+    templates = {
+      folder = 'Templates',
+      date_format = '%Y-%m-%d-%a',
+      time_format = '%H:%M',
+      substitutions = {
+        yesterday = function()
+          return os.date('%Y-%m-%d', os.time() - 86400)
+        end,
+      },
+    },
+    workspaces = {
+      {
+        name = 'personal',
+        path = '~/Notes',
+      },
+    },
+    picker = {
+      name = 'mini.pick',
+    },
+  })
+end)
+
 later(function()
   add({ source = 'kdheepak/lazygit.nvim'})
 end)
@@ -352,10 +505,10 @@ later(function()
   local hi_words = MiniExtra.gen_highlighter.words
   hipatterns.setup({
     highlighters = {
-      fixme = hi_words({ 'FIXME', 'Fixme', 'fixme' }, 'MiniHipatternsFixme'),
-      hack = hi_words({ 'HACK', 'Hack', 'hack' }, 'MiniHipatternsHack'),
-      todo = hi_words({ 'TODO', 'Todo', 'todo' }, 'MiniHipatternsTodo'),
-      note = hi_words({ 'NOTE', 'Note', 'note' }, 'MiniHipatternsNote'),
+      fixme = hi_words({ 'FIXME' }, 'MiniHipatternsFixme'),
+      hack = hi_words({ 'HACK' }, 'MiniHipatternsHack'),
+      todo = hi_words({ 'TODO' }, 'MiniHipatternsTodo'),
+      note = hi_words({ 'NOTE', 'XXX'}, 'MiniHipatternsNote'),
 
       hex_color = hipatterns.gen_highlighter.hex_color(),
     },
@@ -440,7 +593,16 @@ later(function() require('mini.misc').setup() end)
 -- later(function() require('mini.move').setup() end)
 later(function() require('mini.operators').setup() end)
 later(function() require('mini.pairs').setup() end)
-later(function() require('mini.pick').setup() end)
+later(function() require('mini.pick').setup({
+  mappings = {
+      sys_paste = {
+        char = "<M-v>",
+        func = function()
+          MiniPick.set_picker_query({ vim.fn.getreg("+") })
+        end,
+      },
+  }
+}) end)
 later(function() require('mini.snippets').setup() end)
 later(function() require('mini.splitjoin') end)
 later(function() require('mini.surround').setup() end)
