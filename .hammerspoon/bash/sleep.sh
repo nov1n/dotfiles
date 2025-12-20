ab59a48e66d7e599254a182da31a383af8eb9ded
Robert Carosi <robert@carosi.nl>
2025-12-20 13:27:52 +0100

    Fix entity name

--- .hammerspoon/bash/sleep.sh
+++ .hammerspoon/bash/sleep.sh
@@ -9,5 +9,5 @@ exit_if_not_at_desk
 echo "Turning off DAC, speakers, and lights..."
 #retry /opt/homebrew/bin/hass-cli service call switch.turn_off --arguments entity_id=switch.desk_speakers
 #sleep 2
-#retry /opt/homebrew/bin/hass-cli service call light.turn_off --arguments entity_id=light.desk_switch
+#retry /opt/homebrew/bin/hass-cli service call light.turn_off --arguments entity_id=switch.desk_switch
 #retry /opt/homebrew/bin/hass-cli service call light.turn_off --arguments entity_id=light.monitor_led_strip
--- .hammerspoon/bash/wakeup.sh
+++ .hammerspoon/bash/wakeup.sh
@@ -19,7 +19,7 @@ echo "Starting wake-up sequence..."
 hass-cli service call automation.reload # Stops the 'Wake up' automation
 hass-cli service call script.turn_off --arguments entity_id=script.new_script # Stops the 'Wake up light simulator'
 hass-cli service call light.turn_on --arguments entity_id=light.monitor_led_strip
-hass-cli service call light.turn_on --arguments entity_id=light.desk_switch
+hass-cli service call light.turn_on --arguments entity_id=switch.desk_switch
 hass-cli service call switch.turn_on --arguments entity_id=switch.desk_speakers
 hass-cli service call input_boolean.turn_off --arguments entity_id=input_boolean.sleeping
 hass-cli service call light.turn_off --arguments entity_id=light.night_lamp
