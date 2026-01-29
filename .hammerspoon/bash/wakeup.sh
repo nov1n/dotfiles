a46e839597393757795e0f883054c88efd1e53e7
Robert Carosi <robert@carosi.nl>
2026-01-29 12:38:05 +0100

    Turn marantz volume to 0 on wakeup

--- .hammerspoon/bash/wakeup.sh
+++ .hammerspoon/bash/wakeup.sh
@@ -16,13 +16,14 @@ if ! is_at_desk; then
 fi
 
 echo "Starting wake-up sequence..."
-hass-cli service call automation.reload # Stops the 'Wake up' automation
+hass-cli service call automation.reload                                       # Stops the 'Wake up' automation
 hass-cli service call script.turn_off --arguments entity_id=script.new_script # Stops the 'Wake up light simulator'
 hass-cli service call light.turn_on --arguments entity_id=light.monitor_led_strip
 hass-cli service call switch.turn_on --arguments entity_id=switch.desk_switch
 hass-cli service call switch.turn_on --arguments entity_id=switch.desk_speakers
 hass-cli service call input_boolean.turn_off --arguments entity_id=input_boolean.sleeping
 hass-cli service call light.turn_off --arguments entity_id=light.night_lamp
+hass-cli service call media_player.volume_set --arguments entity_id=media_player.marantz_cinema_70s,volume_level=0
 hass-cli service call media_player.turn_off --arguments entity_id=media_player.marantz_cinema_70s
 shortcuts run "Turn off Sleep Mode"
 echo "Finished wake-up sequence."
