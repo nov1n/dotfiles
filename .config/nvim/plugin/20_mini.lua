1acb8635eeefd43112f644e837e25847d18c5c12
Robert Carosi <robert@carosi.nl>
2026-04-02 10:19:50 +0200

    Enable mini.jump2d

--- .config/nvim/plugin/20_mini.lua
+++ .config/nvim/plugin/20_mini.lua
@@ -20,6 +20,7 @@ require("mini.indentscope").setup({ -- Visualizes indent scope
   },
   symbol = "│",
 })
+-- require('mini.animate').setup({}) -- Cursor / scroll animations
 require("mini.map").setup() -- Minimap for code navigation
 require("mini.sessions").setup() -- Session management
 -- require('mini.starter').setup()  -- Shows 'dashboard'
@@ -220,13 +221,13 @@ vim.api.nvim_create_autocmd("User", {
 })
 
 require("mini.git").setup() -- Git integration
--- Jump to any position with 2 keystrokes
--- local jump2d = require("mini.jump2d")
--- jump2d.setup({
---   spotter = jump2d.gen_spotter.pattern("[^%s%p]+"),
---   labels = "asdfghjkl",
---   view = { dim = true, n_steps_ahead = 2 },
--- })
+local jump2d = require("mini.jump2d")
+jump2d.setup({
+  spotter = jump2d.gen_spotter.pattern("[^%s%p]+"),
+  labels = "asdfjkl",
+  view = { dim = true, n_steps_ahead = 2 },
+
+})
 
 require("mini.keymap").setup() -- Keymap utilities
 require("mini.misc").setup()
